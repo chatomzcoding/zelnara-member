705c6cb7b3f3e3e0f5c68f0aca73031032151d27
chatomzcoding <coding.chatomz@gmail.com>
2024-01-01 22:45:03 +0700

    add index awal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomepageController::class,'index']);
 
 Route::middleware([
     'auth:sanctum',
