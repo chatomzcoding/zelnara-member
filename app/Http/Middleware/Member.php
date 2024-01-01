87a74027281c87b20b899d939e41bffb481cf819
chatomzcoding <coding.chatomz@gmail.com>
2024-01-01 22:32:23 +0700

    add middleware member

---
+++ app/Http/Middleware/Member.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Member
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = User::find(Auth::user()->id);
+        if ($user->member) {
+            return $next($request);
+        } else {
+            return redirect('/dashboard');
+        }
+    }
+}
