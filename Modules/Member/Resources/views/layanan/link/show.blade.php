72184db73eecddfbc5b9bc7ef6675181f8e0d149
chatomzcoding <coding.chatomz@gmail.com>
2024-01-03 10:33:09 +0700

    breadcum to link

--- Modules/Member/Resources/views/layanan/link/show.blade.php
+++ Modules/Member/Resources/views/layanan/link/show.blade.php
@@ -11,7 +11,7 @@
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{ url('/dashboard')}}">Dashboard</a></li>
                             <li class="breadcrumb-item"><a href="{{ url('/member/layanan')}}">Layanan</a></li>
-                            <li class="breadcrumb-item"><a href="{{ url('/member/layanan/link')}}">Zelnara Link</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('/member/layanan/'.$linkmaster->layanan->kode)}}">{{ $linkmaster->layanan->nama }}</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Detail Link</li>
                         </ol>
                     </nav>
