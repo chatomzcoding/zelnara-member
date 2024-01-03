6de30e465bf5be0d1fdc9168e47d9b60d220f36e
chatomzcoding <coding.chatomz@gmail.com>
2024-01-04 04:58:00 +0700

    add mzheader to link layanan

--- Modules/Member/Resources/views/layanan/link/index.blade.php
+++ Modules/Member/Resources/views/layanan/link/index.blade.php
@@ -1,22 +1,12 @@
 <x-mazer-layout title="Data Layanan" menu="layanan">
     <div class="page-heading">
-        <div class="page-title">
-            <div class="row">
-                <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>{{ $layanan->nama }}</h3>
-                    <p class="text-subtitle text-muted">Layanan {{ $layanan->link }}</p>
-                </div>
-                <div class="col-12 col-md-6 order-md-2 order-first">
-                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ url('/dashboard')}}">Dashboard</a></li>
-                            <li class="breadcrumb-item"><a href="{{ url('/member/layanan')}}">Layanan</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Detail</li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <x-mzheader
+            judul="{{ $layanan->nama }}"
+            deskripsi="Layanan {{ $layanan->link }}"
+            :link="['/member/layanan' => 'Layanan']"
+            halaman="detail">
+        </x-mzheader>
+        
         <section class="section">
             <div class="card">
                 <div class="card-body">
