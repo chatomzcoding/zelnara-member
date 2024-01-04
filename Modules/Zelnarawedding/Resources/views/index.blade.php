9e32dafb8c3184a3284b3550194d678481765558
chatomzcoding <coding.chatomz@gmail.com>
2024-01-04 09:23:35 +0700

    add module zelnara wedding

---
+++ Modules/Zelnarawedding/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('zelnarawedding::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('zelnarawedding.name') !!}
+    </p>
+@endsection
